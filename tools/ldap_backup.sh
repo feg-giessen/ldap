75f54062cf33f22d539756207ee7363a62f62ee1
Peter Schuster <development@peschuster.de>
2016-09-09 22:51:21 +0200

    Separate config and data
    slapadd requires separate files.

--- tools/ldap_backup.sh
+++ tools/ldap_backup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # dump the ldap database
 
-OUTFILE=ldapdb-`/bin/date +%d-%m-%y_%H-%M`.ldif
+OUTFILE_CONFIG=/var/www/vhosts/feg-giessen.de/backups/ldapdb-`/bin/date +%d-%m-%y_%H-%M`_config.ldif
+OUTFILE_DATA=/var/www/vhosts/feg-giessen.de/backups/ldapdb-`/bin/date +%d-%m-%y_%H-%M`_data.ldif
 
 #echo "Starting slapcat..."
-/usr/sbin/slapcat -n0 > $OUTFILE && /usr/sbin/slapcat -n1 >> $OUTFILE
-gzip -9 $OUTFILE
-find `dirname $OUTFILE` -name "*.ldif" -mtime +30 -exec rm -f {} \;
-find `dirname $OUTFILE` -name "*.ldif.gz" -mtime +30 -exec rm -f {} \;
+/usr/sbin/slapcat -n0 > $OUTFILE_CONFIG && /usr/sbin/slapcat -n1 >> $OUTFILE_DATA
+gzip -9 $OUTFILE_CONFIG
+gzip -9 $OUTFILE_DATA
+find `dirname $OUTFILE_CONFIG` -name "*.ldif" -mtime +30 -exec rm -f {} \;
+find `dirname $OUTFILE_CONFIG` -name "*.ldif.gz" -mtime +30 -exec rm -f {} \;
 STATE=$?
 exit $STATE
